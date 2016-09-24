e434695dc0cd7f59484284df61508bf0a93fea08
Mohit Sardar <mohits19j95@gmail.com>
2016-09-24 16:39:26 -0400

    Removed duplicate classes

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -12,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924173146) do
+ActiveRecord::Schema.define(version: 20160924175525) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email"
@@ -21,7 +20,6 @@ ActiveRecord::Schema.define(version: 20160924173146) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "rooms", force: :cascade do |t|
     t.integer  "roomnumber"
@@ -32,4 +30,3 @@ ActiveRecord::Schema.define(version: 20160924173146) do
   end
 
 end
-
