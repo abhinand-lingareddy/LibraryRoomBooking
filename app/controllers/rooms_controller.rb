c37544dc6a2fb3a54aeeffd3a9c01c72e25486a8
stiruma <stiruma@ncsu.edu>
2016-09-25 03:24:47 +0530

    Rooms list index page added.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
   def new
     @room = Room.new
   end
@@ -11,6 +15,13 @@ class RoomsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+
+    redirect_to rooms_url
   end
 
   def show
