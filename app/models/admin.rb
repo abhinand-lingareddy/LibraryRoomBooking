9868901bd1f91d448d32382dfb32f92c6047921d
Mohit Sardar <mohits19j95@gmail.com>
2016-09-24 16:24:03 -0400

    Added uniquness validation for email of admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ActiveRecord::Base
-  validates :email, :presence => true, format: { with: /[a-z 0-9]*@[a-z 0-9]*/, message: "only allows valid email accounts" }
+  validates :email, :presence => true, uniqueness: true
   validates :name, :presence => true
-  validates :password, :presence => true, length: {minimum: 6}
+  validates :password, :presence => true
 end
