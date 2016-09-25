3c162b2c05f79c1a1b168334e8cabf6829cc12c6
Mohit Sardar <mohits19j95@gmail.com>
2016-09-25 14:28:51 -0400

    Added pre-configured Admin

--- db/migrate/20160925071315_create_users.rb
+++ db/migrate/20160925071315_create_users.rb
@@ -8,5 +8,7 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+       User.create(:email=>"msardar@ncsu.edu", :name=>"Mohit S", :password=>"123", :admin=>true)
   end
 end
