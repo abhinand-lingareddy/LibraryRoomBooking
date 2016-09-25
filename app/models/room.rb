7679a73b72945706f92a153d63c242cadb95904e
Mohit Sardar <mohits19j95@gmail.com>
2016-09-25 19:04:17 -0400

    Minor changes

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ActiveRecord::Base
   has_many :bookings
   enum buildingname: [ :hunt, :hill ]
-  enum capacity: [ :small, :meduim, :large ]
+  enum capacity: [ :small, :medium, :large ]
 
   def self.search(field,search)
       where("#{field}": "#{search}")
