f3f5725a504c3b6e295d7ac1686d1824440bc40e
Mohit Sardar <mohits19j95@gmail.com>
2016-09-24 22:40:48 -0400

    minor change

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,4 @@
 class Admin < ActiveRecord::Base
-  attr_reader :email
   validates :email, :presence => true, uniqueness: true
   validates :name, :presence => true
   validates :password, :presence => true
